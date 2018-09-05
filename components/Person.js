3ad440fbda9a5b038200e75846a683396784eae3
Cícero Neto <cicero.alex@gmail.com>
2018-09-05 16:44:44 -0300

    More styles changes

--- components/Person.js
+++ components/Person.js
@@ -1,10 +1,14 @@
 const Person = ({name, location, picture, email, login}) => (
-    <div>
-        <img src={picture.large} />
-        <p>{name.first} {name.last} | @{login.username}</p>
-        <p>{location.city} - {location.state}</p>
-        <p>{email}</p>
+  <div className="person">
+    <div className="image">
+      <img src={picture.large} />
     </div>
+    <div className="info">
+      <p>{name.first} {name.last} | @{login.username}</p>
+      <p>{location.city} - {location.state}</p>
+      <p>{email}</p>
+    </div>
+  </div>
 )
 
 export default Person
--- components/PersonList.js
+++ components/PersonList.js
@@ -3,7 +3,7 @@ import Person from './Person'
 const PersonList = ({people, title}) => (
     <section id={title.toLowerCase().replace(/ /g, "-")}>
         <h1>{title}</h1>
-        <div className="person">
+        <div className="people">
             {people.map((person, index) =>
                 <Person key={index} {...person} />
             )}
